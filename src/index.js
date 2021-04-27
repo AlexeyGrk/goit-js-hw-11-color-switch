b1ac1a5eae5e8b3770c9b9f11645d1815548f437
AlexeyGrk <alexgmirko@gmail.com>
2021-04-27 11:58:39 +0300

    add hw-10

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './sass/main.scss';
 import Darkmode from 'darkmode-js';
+import menu from './menu.json';
+import createCardsTpm from './templates/cards.hbs';
 
 // new Darkmode().showWidget();
 const options = {
-  bottom: '64px', // default: '32px'
+  bottom: '20px', // default: '32px'
   right: '32px', // default: '32px'
   left: 'unset', // default: 'unset'
   time: '0.5s', // default: '0.3s'
@@ -13,8 +15,11 @@ const options = {
   buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: true, // default: true,
   label: '🐥', // default: ''
-  autoMatchOsTheme: true, // default: true
+  autoMatchOsTheme: false, // default: true
 };
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
+const menuGalleryRef = document.querySelector('.js-menu');
+const markup = createCardsTpm(menu);
+menuGalleryRef.insertAdjacentHTML('beforeend', markup);
